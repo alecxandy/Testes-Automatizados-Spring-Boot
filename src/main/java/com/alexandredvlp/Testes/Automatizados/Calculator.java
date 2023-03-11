e9f362364c1cf72dda71b35aafc2b0bc9234f76d
alecxandy <alecxandy@gmail.com>
2023-03-11 14:02:32 -0300

    Inicio do projeto e primeiro teste

---
+++ src/main/java/com/alexandredvlp/Testes/Automatizados/Calculator.java
@@ -0,0 +1,7 @@
+package com.alexandredvlp.Testes.Automatizados;
+
+public class Calculator {
+    public int soma(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/alexandredvlp/Testes/Automatizados/TestesAutomatizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandredvlp.Testes.Automatizados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesAutomatizadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
